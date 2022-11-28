e4aeddbb9f21bb2340435cb3f82df12887a21dd5
Skye Brady <skye@blueskye.co.uk>
2022-11-28 16:12:55 +0000

    Only spawn fish at start of sim

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,15 @@ import { FpsCounter } from './entities/fpsCounter'
 import { FishSpawner } from './entities/fishSpawner'
 import { EntityCounter } from './entities/entityCounter'
 import { AlgaeSpawner } from './entities/algaeSpawner'
+import { Age, MaxAge } from './components'
 
 const run = (): void => {
   const world = new World()
-  world.spawnEntity(new FishSpawner(world, 400))
-  world.spawnEntity(new AlgaeSpawner(world, 100))
+  const fishSpawner = new FishSpawner(world, 0)
+  fishSpawner.attachComponent(new Age(fishSpawner))
+    .attachComponent(new MaxAge(fishSpawner, 10))
+  world.spawnEntity(fishSpawner)
+  world.spawnEntity(new AlgaeSpawner(world, 1000/3))
   world.spawnEntity(new EntityCounter(world))
   world.spawnEntity(new FpsCounter(world));
   (<any>window).world = world
